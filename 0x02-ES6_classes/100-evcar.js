fe42dfd8865d3c81e51a00492e31e31fc7c96609
Navygeta <tinashenavonika@gmail.com>
2024-04-26 00:41:10 +0200

    ev car 2

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,13 +1,14 @@
-/* eslint-disable class-methods-use-this */
 import Car from './10-car';
 
-export default class EVCar extends Car {
-  constructor(brand, motor, color, range) {
+class EVCar extends Car {
+  constructor(brand = '', motor = '', color = '', range = '') {
     super(brand, motor, color);
     this._range = range;
   }
 
-  cloneCar() {
-    return new Car();
+  static get [Symbol.species]() {
+    return Car;
   }
 }
+
+export default EVCar;
