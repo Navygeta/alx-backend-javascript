2732332e7878e5f62aada390cae10c4b58a81aa8
Navygeta <tinashenavonika@gmail.com>
2024-06-19 04:00:50 +0200

    node js basic

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,15 @@
+/**
+ * Logs a message to the console.
+ * @param {string} message - The message to log.
+ * @returns {void}
+ */
+// Arrow function for logging a message to the console
+const displayMessage = (message) => console.log(message);
+
+/**
+ * Exports the displayMessage function as the module's default export
+ * @module displayMessage
+ * @function
+ */
+// Default module export.
+module.exports = displayMessage;
